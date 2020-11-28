f01647b681299ddfbc8c21454208eefe322af5c1
贺释贤 <he_shx@hisuntech.com>
2020-11-28 20:20:28 +0800

    设计模式工程搭建

---
+++ src/main/java/com/hezhuma/designpattern/DesignPatternApplication.java
@@ -0,0 +1,16 @@
+package com.hezhuma.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heshixian
+ */
+@SpringBootApplication
+public class DesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternApplication.class, args);
+    }
+
+}
